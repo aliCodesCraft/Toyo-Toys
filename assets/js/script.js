2b0b25b25d54e196609145966cadc7523aeac1b7
aliCodesCraft <apt00ali@gmail.com>
2025-01-15 17:02:36 +0500

    responsive navbar added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".bars").click(function(){
+        $(".mobile-container").show(300);
+    })
+
+    $(".cross-nav").click(function(){
+        $(".mobile-container").hide(300);
+    })
+})
