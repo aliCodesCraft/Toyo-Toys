d89adac34cbcabb8ae888481a39bc8143caee88c
aliCodesCraft <apt00ali@gmail.com>
2025-01-20 11:57:05 +0500

    80% HomePage Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
         $(".mobile-container").hide(300);
     })
 })
+
+$(document).ready(function(){
+    $(".addToCart").click(function(){
+        alert("added to cart")
+        $(".fa-solid fa-bag-shopping").toggleClass(".fa-solid fa-heart");
+    })
+})
+
+$(document).ready(function(){
+    $(".myCart").click(function(){
+        $(".cart-container").toggle(150);
+    })
+    $(".closeCart").click(function(){
+        $(".cart-container").hide(150);
+    })
+})
