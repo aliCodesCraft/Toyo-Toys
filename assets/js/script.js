d914109eb2ee847de01118d6e20d1d3efc277832
aliCodesCraft <apt00ali@gmail.com>
2025-01-25 22:04:45 +0500

    70% done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,65 @@
-$(document).ready(function(){
-    $(".bars").click(function(){
+$(document).ready(function () {
+    $(".bars").click(function () {
         $(".mobile-container").show(300);
     })
 
-    $(".cross-nav").click(function(){
+    $(".cross-nav").click(function () {
         $(".mobile-container").hide(300);
     })
 })
 
-$(document).ready(function(){
-    $(".addToCart").click(function(){
-        alert("added to cart")
-        $(".fa-solid fa-bag-shopping").toggleClass(".fa-solid fa-heart");
-    })
-})
-
-$(document).ready(function(){
-    $(".myCart").click(function(){
+$(document).ready(function () {
+    $(".myCart").click(function () {
         $(".cart-container").toggle(150);
     })
-    $(".closeCart").click(function(){
+    $(".closeCart").click(function () {
         $(".cart-container").hide(150);
     })
 })
+
+$(document).ready(function () {
+    let totalPrice = 0;
+    
+    $(".addToCart").click(function () {
+        let myCard = $(this).closest(".mycard");
+
+        let itemPic = myCard.find("img").attr("src");
+        let itemName = myCard.find("a.title").text();
+        let itemPrice = parseFloat( myCard.find(".price p").text().replace("PKR", "").trim());
+
+        let item = `        <div class="cart-content">
+            <div class="cart-pic">
+                <img src="${itemPic}" alt="">
+            </div>
+            <div class="cart-info">
+                <p>${itemName}</p>
+            </div>
+            <div class="cart-total">
+                <p>PKR ${itemPrice}</p>
+            </div>
+            <div class="del">
+                <i class="fa-solid fa-trash"></i>
+            </div>
+        </div>
+        `;
+        $(".cart-box").append(item)
+        alert("added to cart");
+
+    // Total Price
+    totalPrice += itemPrice;
+    $("#total").text(totalPrice.toFixed(2));
+    });
+
+    // Removing Items
+    $(".cart-box").on("click", ".del", function () {
+        let itemPrice = parseFloat($(this).closest(".cart-content").find(".cart-total p").text());
+        
+        totalPrice -= itemPrice; 
+        $("#total").text(totalPrice.toFixed(2));
+    
+        $(this).closest(".cart-content").remove();
+    });
+    
+});
+
+
