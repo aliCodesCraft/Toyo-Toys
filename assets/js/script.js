dca9ce9b0d95fa880c1551ab114e49ceb35c67b2
aliCodesCraft <apt00ali@gmail.com>
2025-01-31 11:35:24 +0500

    detail page added1

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,13 @@ $(document).ready(function () {
 $(document).ready(function () {
     let totalPrice = 0;
     let itemCount = 0;
-    
+
     $(".addToCart").click(function () {
         let myCard = $(this).closest(".mycard");
 
         let itemPic = myCard.find("img").attr("src");
         let itemName = myCard.find("a.title").text();
-        let itemPrice = parseFloat( myCard.find(".price p").text().replace("PKR", "").trim());
+        let itemPrice = parseFloat(myCard.find(".price p").text().replace("PKR", "").trim());
 
         let item = `        <div class="cart-content">
             <div class="cart-pic">
@@ -46,12 +46,12 @@ $(document).ready(function () {
         $(".cart-box").append(item)
         alert("added to cart");
 
-    // Total Price
-    totalPrice += itemPrice;
-    $("#total").text(totalPrice.toFixed(2));
+        // Total Price
+        totalPrice += itemPrice;
+        $("#total").text(totalPrice.toFixed(2));
 
-    itemCount++;
-    $(".count").text(itemCount);
+        itemCount++;
+        $(".count").text(itemCount);
 
     });
 
@@ -59,21 +59,55 @@ $(document).ready(function () {
     $(".cart-box").on("click", ".del", function () {
         let itemPrice = parseFloat($(this).closest(".cart-content").find(".cart-total p").text().replace("PKR", "").trim());
 
-        
-        totalPrice -= itemPrice; 
+
+        totalPrice -= itemPrice;
         $("#total").text(totalPrice.toFixed(2));
-    
+
         $(this).closest(".cart-content").remove();
 
         itemCount--;
         $(".count").text(itemCount);
     });
-    
+
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {  // For small screens (mobile)
+            slidesPerView: 1,
+        },
+        480: {  // Slightly larger screens
+            slidesPerView: 1,
+        },
+        768: {  // Tablets
+            slidesPerView: 2,
+        },
+        1024: { // Laptops
+            slidesPerView: 3,
+        },
+        1200: { // Large screens
+            slidesPerView: 4,
+        }
+    }
+
 });
 
 
+let mainImage = document.getElementById("mainImage")
 
-
+function changeImage(smallImage) {
+    mainImage.src = smallImage.src;
+}
 
 
 
